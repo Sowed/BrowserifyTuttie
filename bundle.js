ab9ed8d9befcb9ba48c4df3c3ff0f0c669ab8cd4
iamcastelli <sowed@cyberdude.com>
2017-05-03 18:33:59 +0300

    Add `beefy`
    Enable live reload alongside automatic rebuild with
    `http://didact.us/beefy/` adding it as a dev dependency with `npm
    install --save-dev beefy` Helps in automatic browser loading while
    changes are made to target file.
    Run `beefy app.js --live` in a new instance of the CLI window

--- bundle.js
+++ bundle.js
@@ -4,7 +4,7 @@ let R = require('ramda')
 let square = function square(x) {
         return x * x;
     },
-    squares = R.chain(square, [1, 2, 3, 4, 5]);
+    squares = R.chain(square, [1, 2, 3, 4, 5,6,7,8,9,10]);
 document.getElementById('response').innerHTML = squares;
 
 },{"ramda":2}],2:[function(require,module,exports){
