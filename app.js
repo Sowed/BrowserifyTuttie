48f9d7296b486941bcdae8a25cb182ce8e8fc704
iamcastelli <sowed@cyberdude.com>
2017-05-04 01:44:40 +0300

    Add minifyify build
    `npm install --save-dev minifyify`
    Minify code on build. Add a script (`build-min`) `watchify app.js -o
    bundle.js -p [minifyify --map bundle.js.map --output bundle.js.map] -d
    -v` to watch and minify the code.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
-let R = require('ramda')
-let square = function square(x) {
+let R = require('ramda'),
+    square = function square(x) {
         return x * x;
     },
-    squares = R.chain(square, [1, 2, 3, 4, 5,6,7,8,9,10]);
+    squares = R.chain(square, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 document.getElementById('response').innerHTML = squares;
