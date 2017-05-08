b507ff0225ecc8a9f34691283fb51c56c3c1fde2
iamcastelli <sowed@cyberdude.com>
2017-05-08 17:49:34 +0300

    Update Gulp tasks
    Tweak tasks for optimisation, like ignoring already worked on images and
    scripts, uglification, global error logging with plumber, task
    dependence management with run-seqence
    Ignore public folder/same as build folder auto generated  by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -20,9 +20,11 @@ const babelify = require('babelify'), //Converts ES6 & JSX to ES5
     gutil = require('gulp-util'), //Provides gulp utilities like logging and beep
     imagemin = require('gulp-imagemin'), //Utility for optimising images
     merge = require('utils-merge'), //Object merge tool
+    newer = require('gulp-newer'), //Pass only newer source files than corresponding destination files.
     notify = require('gulp-notify'), //Provides notification to both console and Growel
     plumber = require('gulp-plumber'), //Prevents gulp.watch from crashing with error log
     rename = require('gulp-rename'), //Renames source files
+    runSequence = require('run-sequence'), //Runs a sequence of gulp tasks in the specified order.
     source = require('vinyl-source-stream'), //Adds Vinyl stream support
     sourcemaps = require('gulp-sourcemaps'), //Provides external sourcemap files
     uglify = require('gulp-uglify'), //Minifies files
@@ -30,15 +32,26 @@ const babelify = require('babelify'), //Converts ES6 & JSX to ES5
 
 //A Gulp congiguration
 let config = {
+    baseDir: './',
+    indexUrl: './index.html',
     js: {
-        src: './js/app.js', //App Entry point
+        src: './js/*.js', //JS files source
+        app: './js/app.js', //App Entry point
         watch: './js/**/*', //Watch all Javascript Files
-        outputDir: './build/js/', //Directory to save bundles
+        dist: './dist/js/', //Directory to save bundles
         mapDir: 'maps/', //Sub Dir to save sourcemaps to
-        outputFile: 'bundle.js' //Name of built file
+        outputFile: 'bundle.js', //Name of built file
+        outputDir: './build/js/', //Directory to save bundles
+        vendor: [ //Vendor Scripts to be concatenated into build file
+            './app/js/vendor/jquery-3.2.1.min.js',
+            './app/js/vendor/tether.min.js',
+            './app/js/vendor/bootstrap.min.js'
+        ],
+        allMinJs: 'build.min.js', //Name of minified build with vendor js
+        minBuildDir: './public/js/', //Dir of all cocatenated scripts
     },
     img: {
-        src: './app/images/*.+(png|jpg|gif)', //Path to image assets
+        src: './app/images/*.+(png|jpg|gif|svg)', //Path to image assets
         dst: './build/img' //Path to optimised images
     }
 };
@@ -47,7 +60,7 @@ let config = {
 function mapError(err) {
     if (err.fileName) {
         //Regular error
-        gutil.log(chalk.red(err.name) + ': ' + chalk.yellow(err.fileName.replace(__dirname + '/src/js/', '')) + ': ' + 'Line ' + chalk.magenta(err.lineNumber) + '& ' + 'Column ' + chalk.magenta(err.columnNumber || err.column) + ': ' + chalk.cyan(err.description));
+        gutil.log(chalk.red(err.name) + ': ' + chalk.yellow(err.fileName.replace(__dirname + config.js.app, '')) + ': ' + 'Line ' + chalk.magenta(err.lineNumber) + '& ' + 'Column ' + chalk.magenta(err.columnNumber || err.column) + ': ' + chalk.cyan(err.description));
     } else {
         //Browserify Error
         gutil.log(chalk.red(err.name) + ': ' + chalk.yellow(err.message));
@@ -55,41 +68,19 @@ function mapError(err) {
 }
 
 /**
- * Complete the final file outputs
- * This makes it easy to use common bundling options in different tasks
+ * Adding a global plumber to catch piped errors on gulp
+ * Overriding #src and binding to gulp-notify is pretty simple and useful
+ * @ref: https://gist.github.com/floatdrop/8269868#gistcomment-1428507
  */
-function bundle(bundler) {
-    let bundleTimer = duration('JavaScript bundle time');
-
-    //Add options to add 'base' bundler added as a parameter
-    bundler
-        .bundle() //Start the bundle
-        .on('error', mapError) //Map error reporting
-        .pipe(source(config.js.src)) //Set the source name/entry point
-        .pipe(buffer()) //Convert to gulp pipeline
-        .pipe(rename(config.js.outputFile)) //Rename the output file from app.js
-        .pipe(sourcemaps.init({ loadMaps: true })) //Extract inline sourcemaps
-        .pipe(sourcemaps.write(config.js.mapDir)) //Save sourcemap to another output folder
-        .pipe(gulp.dest(config.js.outputDir)) //Set the build output folder
-        .pipe(notify({ message: 'Generated File: <% file.relative %>' })) //Output the file being created
-        .pipe(bundleTimer) //Output time of file creation
-        .pipe(browserSync.stream()); //Sync the browsers and livereload
-}
-
-/**
- * Adding a global pluber to catch piped errors on gulp
- * I found overriding #src and binding to gulp-notify to be pretty simple and useful
-* @ref: https://gist.github.com/floatdrop/8269868#gistcomment-1428507
-*/
 let _gulpsrc = gulp.src;
 gulp.src = function() {
     return _gulpsrc.apply(gulp, arguments)
         .pipe(plumber({
-            errorHandler: function(err) {
+            errorHandler: (err) => {
                 notify.onError({
                     title: "Gulp Error",
                     message: "Error: <%= error.message %>",
-                    sound: "Bottle"
+                    sound: "pop"
                 })(err);
                 this.emit('end');
             }
@@ -97,32 +88,21 @@ gulp.src = function() {
 };
 
 /**
- * Start on a fresh clean slate, delete the old build if any
+ * To start on a fresh clean slate, delete any auto generated files
+ * Like the old build and optimised public assets if any
  */
-gulp.task('clean', function() {
+gulp.task('clean', () => {
     // You can use multiple globbing patterns as you would with `gulp.src`
-    return del(['build']);
-});
-
-/**
- * Task to initialize Browsersync, for browser livereloading and sync
- * Update this to run after guild task!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- */
-gulp.task('browser-sync', () => {
-    console.log('browser-syncing');
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
+    return del(['build', 'public']);
 });
 
 /**
  * Task to Minify and Optimise images
  */
-gulp.task('imagemin', ['clean'], () => {
+gulp.task('imagemin', () => {
     return gulp.src(config.img.src)
         .pipe(changed(config.img.dst))
+        //Alternative -> .pipe(newer(config.img.dst))
         .pipe(imagemin([
             imagemin.gifsicle({ interlaced: true }),
             imagemin.jpegtran({ progressive: true }),
@@ -133,91 +113,31 @@ gulp.task('imagemin', ['clean'], () => {
 });
 
 /**
- * Process the JS and return a stream
- */
-gulp.task('js', ['clean'], () => {
-    return gulp.src(config.js.src)
-        .pipe(sourcemaps.init())
-        .pipe(browserify())
-        .pipe(uglify())
-        .pipe(concat('bundle.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(config.js.outputDir));
-});
-
-/**
- * Add a watch task with livereloading
- */
-gulp.task('watch', ['browser-sync'], () => {
-    gulp.watch('*.html').on('change', browserSync.reload);
-    gulp.watch(config.js.src, ['js']);
-    gulp.watch(config.img.src., ['images']);
-});
-
-/**
- * Just a simple build task example
- */
-gulp.task('build', () => {
-    let bundler = browserify(config.js.src) //Pass browserify entry point
-        .transform(babelify, { presets: ['es2015', 'react'] });
-
-    bundle(bundler); //Can chanin other options, eg sourcemaps, rename, uglify, watchify, livereload, etc
-});
-
-/**
- * A sample default gulp task for the build 
- */
-gulp.task('default', ['watch'], () => {
-    let args = merge(watchify.args, { debug: true }); //Merge in defualt watchify args with browserify args
-
-    let bundler = browserify(config.js.src, args) //Browserify
-        .plugin(watchify, { ignoreWatch: ['**/node-modules/**', '**/bower_components/**'] }) //Setup watchify to watch source changes
-        .transform(babelify, { presets: ['es2015', 'react'] }); //Add babel transforms
-
-    bundle(bundler); //Run the bundle the first time (Required for watchify to exec)
-
-    bundler.on('update', function() {
-        bundle(bundler); //Rerun bundle on source updates
-    });
-});
-
-/**
- * Another sample default gulp task 
- */
-gulp.task('default-ex2', ['watch', 'js', 'imagemin']);
-
-
-
-/**
+ * Alternatively using an gulp-newer instead of gulp-changed
  * @ref https://github.com/tschaub/gulp-newer
  * A Gulp plugin for passing through only those source files that are newer than corresponding destination files.
  * Using newer with 1:1 source:dest mappings. 
- * The default task in the example below sets up a watch that minifies images on changes. 
+ * The default task(default-ex0) in the example-0 below sets up a watch that minifies images on changes. 
  * Piping the source files to newer before imagemin ensures that only those images that have 
  * changed are minified. The newer plugin is configured with the directory path for minified images.
  */
 //================================================================================================
-
-var gulp = require('gulp');
-var newer = require('gulp-newer');
-var imagemin = require('gulp-imagemin');
-
-var imgSrc = 'src/img/**';
-var imgDest = 'build/img';
-
 // Minify any new images
-gulp.task('images', function() {
-
+gulp.task('images-newer', () => {
     // Add the newer pipe to pass through newer images only
-    return gulp.src(imgSrc)
-        .pipe(newer(imgDest))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDest));
-
+    return gulp.src(config.img.src)
+        .pipe(newer(config.img.dst)) //Testing alternative to changed()
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({ plugins: [{ removeViewBox: true }] })
+        ], { verbose: true }))
+        .pipe(gulp.dest(config.img.dst));
 });
-
-gulp.task('default', function() {
-    gulp.watch(imgSrc, ['images']);
+gulp.task('watch-img', ['images-newer'], () => {
+    //Watch the image directory and optimise any new images
+    gulp.watch(config.img.src, ['images-newer']);
 });
 
 /**
@@ -227,18 +147,201 @@ gulp.task('default', function() {
  * is newer than the destination file. The newer plugin is configured with the destination file path.
  */
 //================================================================================================
-
-var gulp = require('gulp');
-var newer = require('gulp-newer');
-var concat = require('gulp-concat');
-
-// Concatenate all if any are newer
-gulp.task('concat', function() {
-
-    // Add the newer pipe to pass through all sources if any are newer
-    return gulp.src('lib/*.js')
-        .pipe(newer('dist/all.js'))
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'));
-
+// Example task: Process the JS and return a stream, Concatenate all if any are newer
+gulp.task('js-newer', () => {
+    return gulp.src(config.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(newer(config.js.outputDir))
+        .pipe(concat(config.js.outputFile))
+        .pipe(sourcemaps.write(config.js.mapDir))
+        .pipe(gulp.dest(config.js.outputDir));
 });
+
+/**
+ * Make a global reuseable Browserify bundle, Extract any reuable methods
+ * This makes it easy to use common bundling options in different tasks 
+ */
+function browserifyBundle(bundler) {
+    let bundleTime = duration('Browserification completed in... ');
+    bundler.bundle()
+        .pipe(plumber({
+            errorHandler: (err) => {
+                notify.onError({
+                    title: "Gulp Error",
+                    message: "Error: <%= error.message %>",
+                    sound: "pop"
+                })(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(source(config.js.outputFile))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .on("error", gutil.log)
+        .pipe(sourcemaps.write(config.js.mapDir))
+        .pipe(gulp.dest(config.js.outputDir))
+        .pipe(bundleTime)
+        .pipe(browserSync.stream());
+}
+
+/**
+ * Another example of a reusable bundle
+ */
+function bundle(bundler) {
+    let bundleTimer = duration('JavaScript bundle time');
+
+    //Add options to add 'base' bundler added as a parameter
+    bundler
+        .bundle() //Start the bundle
+        .on('error', mapError) //Map error reporting
+        .pipe(source(config.js.src)) //Set the source name/entry point
+        .pipe(buffer()) //Convert to gulp pipeline
+        .pipe(sourcemaps.init({ loadMaps: true })) //Extract inline sourcemaps
+        .pipe(rename(config.js.outputFile)) //Rename the output file from app.js
+        .pipe(sourcemaps.write(config.js.mapDir)) //Save sourcemap to another output folder
+        .pipe(gulp.dest(config.js.outputDir)) //Set the build output folder
+        .pipe(notify({ message: 'Generated File: <% file.relative %>' })) //Output the file being created
+        .pipe(bundleTimer) //Output time of file creation
+        .pipe(browserSync.stream()); //Sync the browsers and livereload
+}
+
+/**
+ * Javascript module Bundling, and Uglification, note that this uses our first bundle 
+ */
+gulp.task('browserify', () => {
+    const bundler = browserify({
+            entries: config.js.app,
+            debug: true
+        })
+        .plugin(watchify, { ignoreWatch: ['**/node-modules/**', '**/bower_components/**'] }) //Setup watchify to watch source changes
+        .transform('babelify', { presets: ["es2015", 'react'] }); //Add babel transforms
+
+    browserifyBundle(bundler);
+    bundler.on('update', () => {
+        browserifyBundle(bundler);
+    });
+});
+
+//Gulp concatenate vendor files to browserified bundle
+gulp.task('concat-js', () => {
+    let builtJs = config.js.outputDir + config.js.outputFile;
+    console.log(builtJs);
+    //config.js.vendor.push(builtJs) //Add browserified build
+    console.log(config.js.vendor);
+    return gulp.src(config.js.vendor)
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        //.pipe(newer(config.js.minBuildDir)) // Only rewrite if newer
+        .pipe(concat(config.js.allMinJs))
+        .pipe(sourcemaps.write(config.js.mapDir))
+        .pipe(gulp.dest(config.js.minBuildDir))
+        .pipe(browserSync.stream()); //Sync the browsers and livereload;
+});
+
+/*!
+ * @ref: http://stackoverflow.com/questions/22824546/how-to-run-gulp-tasks-sequentially-one-after-the-other
+ * @reformat(): https://www.npmjs.com/package/run-sequence
+ * This is intended to be a temporary solution until the release of gulp 4.0, 
+ * which has support for defining task dependencies in series or in parallel. 
+ * Be aware that this solution is a hack, and may stop working with a future update to gulp.
+ */
+gulp.task('build-js', (done) => {
+    runSequence('browserify', 'concat-js', () => {
+        console.log('JS Files Built...');
+        done();
+    });
+});
+
+/*!
+ * Note that the `build-js task` has a bug...for some reason the `concat-js` task finishes before the `browserify` even if it shouldn't
+ * JS files are concatenated before browserify has completed creating the build file bundle.js. 
+ * This results to no `app.js` code in the final ./public/build.min.js.
+ * A quick hack is rerunning the concat-js task without cleaning the old build. That way it finds the app.js already bundled
+ * NB: Created the public-js task to rerun the code, but gulp ignores repeated tasks in a single task run :-D
+ * Researching on this, FIX the bug
+ */
+//ToDo: Fix bug in build-js task
+gulp.task('public-js', (done) => {
+    runSequence('build-js', () => {
+        done();
+        console.log('Rerun concatenation with assured built bundle.js...');
+        gulp.start('concat-js')
+    });
+}); //Note that this is a vague hack
+
+/**
+ * Task to initialize Browsersync, for browser livereloading and sync
+ */
+//TODO: Update this to run after build task!
+gulp.task('browser-sync', () => {
+    console.log('browser-syncing');
+    browserSync.init({
+        server: {
+            //baseDir: 'index-selfhost.html',
+            baseDir: config.baseDir,
+            index: config.indexUrl
+        }
+    });
+});
+
+/**
+ * Add a watch task, to first clean, perfome a browserify bundling, image optimisation
+ *  and browsersync livereloading. Ideal approch for live/remote apps
+ *  This creates a dev build (./build/), requiring inline vendor scripts
+ *  as they are not added to the bundle
+ */
+gulp.task('devbundle', (done) => {
+    runSequence('clean', ['images-newer', 'browserify'], 'browser-sync', () => { done(); });
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch(config.img.src, ['watch-img']);
+});
+
+/**
+ * Run `devbundle` without cleaning the build file. 
+ * This saves build time especially as the optimised images are not deleted
+ * Most time consuming `images-newer` will ignore the already optimised images
+ */
+gulp.task('devbundle-!clean', (done) => {
+    runSequence(['images-newer', 'browserify'], 'browser-sync', () => { done(); });
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch(config.img.src, ['watch-img']);
+});
+
+/**
+ * Add a watch task, to first clean, perfome a browserify bundling, image optimisation, scripts concatenation
+ *  and browsersync livereloading. This creates a public build (./public/). The Vendor scripts are self contained
+ *  Optimum for offline apps/self hosted apps.
+ *  NB: `index-selfhost.html` file uses the public self contained build without inline vendor scripts
+ */
+gulp.task('selfhost', (done) => {
+    config.baseDir = 'index-selfhost.html'; //Let Browsersync load server with this file
+    runSequence('clean', ['watch-img', 'public-js'], 'browser-sync', () => {
+        console.log('All Vendor Files are Built now');
+        done();
+    });
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch(config.js.src, ['public-js']);
+});
+
+/**
+ * Rerun the `selfhost` task without first cleaning the build and public directories
+ * NB: The ./public `build.min.js` doesnt capture `bundle.js` pipe this task twice.
+ * ToDo: Yet to fix `concat-js` - `browserify` task sequency
+ */
+gulp.task('selfhost-!clean', (done) => {
+    config.indexUrl = 'index-selfhost.html'; //Let Browsersync load server with this file
+    runSequence(['watch-img', 'public-js'], 'browser-sync', () => {
+        console.log('All Vendor Files are Built now');
+        done();
+    });
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch(config.js.src, ['public-js']);
+});
+
+/**
+ * Run the selfhost task by default.d from `browserify`
+ * Resulting into missing `app.js` logic. Quick hack is running 
+ */
+//ToDo: Fix the `build-js` - `concat-js` - `browserify` task sequency 
+gulp.task('default', ['selfhost']);
